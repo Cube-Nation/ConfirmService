153afcf60fc0f3984fd79080cd22c4eaaca0723c
Tristan Cebulla <equinox@lichtspiele.org>
2015-03-28 17:45:37 +0100

    added plugin instance accessor, create default config

--- main/java/org/lichtspiele/ConfirmService/ConfirmServicePlugin.java
+++ main/java/org/lichtspiele/ConfirmService/ConfirmServicePlugin.java
@@ -4,4 +4,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class ConfirmServicePlugin extends JavaPlugin {
 
+	private static ConfirmServicePlugin instance;
+	
+	public void onEnable() {
+		instance = this;
+		
+		this.getDataFolder().mkdirs();
+		this.saveDefaultConfig();
+	}
+	
+	public static ConfirmServicePlugin getInstance() {
+		return instance;
+	}
+	
 }
